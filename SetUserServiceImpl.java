afad5fd74a889575805b68fe868c4db8c100825c
George Davis <trapper.davis@yahoo.com>
2019-12-19 19:38:16 -0500

    Add files via upload

---
+++ SetUserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.mongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mongo.model.User;
+import com.mongo.repositories.UserRepository;
+
+@Service
+public class SetUserServiceImpl implements SetUserService {
+	
+	// Autowire the User Repository
+	@Autowired
+	private UserRepository userRepo;
+	
+
+	@Override
+	public User saveUser(User user) {
+		return userRepo.save(user);
+	}
+
+}
