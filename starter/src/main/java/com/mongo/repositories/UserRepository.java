a0a85c16ff300a9045154f8c1d5886f00e4b5a2f
George Davis <trapper.davis@yahoo.com>
2020-01-06 21:18:56 -0500

    Adding Delete User functionality

--- starter/src/main/java/com/mongo/repositories/UserRepository.java
+++ starter/src/main/java/com/mongo/repositories/UserRepository.java
@@ -2,22 +2,23 @@ package com.mongo.repositories;
 
 import java.util.List;
 
+import com.mongo.model.User;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import com.mongo.collections.Users;
-import com.mongo.model.User;
+public interface UserRepository extends MongoRepository<User, String> {
 
-public interface UserRepository extends MongoRepository<User, Object> {
-	
-//	Users findFirstByName(String name);
-//	
-//	@Query( "{address: '?0'}" )
-//	List<Users> findCustomByAddress(String address);
-//	
-//	@Query("{address : { $regex: ?0 } }")
-//    List<Users> findCustomByRegExAddress(String domain);
-	
-	User findFirstByGameName(String gameName);
+    // Users findFirstByName(String name);
+    //
+    // @Query( "{address: '?0'}" )
+    // List<Users> findCustomByAddress(String address);
+
+    User findUserByLastName(String lastName);
+
+    @Query("{gameName : { $regex: ?0 } }")
+    List<User> findCustomByRegExGameName(String domain);
+
+    User findFirstByGameName(String gameName);
 
 }
--- starter/src/main/java/com/mongo/services/GetUsersServiceImpl.java
+++ starter/src/main/java/com/mongo/services/GetUsersServiceImpl.java
@@ -1,12 +1,30 @@
 package com.mongo.services;
 
+import com.mongo.model.GetUsersRequest;
+import com.mongo.model.GetUsersResponse;
+import com.mongo.model.User;
+import com.mongo.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetUsersServiceImpl implements GetUsersService {
 	
 	// @Autowire the DAO / Mongo Custom Repository
+	@Autowired
+	private UserRepository userRepo;
 	
-	// return the response
+	public ResponseEntity<GetUsersResponse> getUsers(GetUsersRequest request) {
+
+		GetUsersResponse response = new GetUsersResponse();
+		User user = userRepo.findFirstByGameName(request.getGameName());
+		response.setFirstName(user.getFirstName());
+		return new ResponseEntity<GetUsersResponse>(response, HttpStatus.OK);
+
+
+	}
 
 }
